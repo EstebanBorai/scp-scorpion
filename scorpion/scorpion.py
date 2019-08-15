91c162753dbf8bfe6d8a3b7da3e35f9fbd25c241
Esteban Borai <estebanborai@outlook.com>
2019-08-15 01:23:53 -0300

    Add base implementation

--- scorpion/scorpion.py
+++ scorpion/scorpion.py
@@ -6,6 +6,12 @@ config_file = f'{current_dir}/scorpion.json'
 is_first_time = False
 
 def get_config():
+	"""
+		Attempt to gather configuration from the
+		current working directory.
+		If the file doesn't exists creates a new
+		configuration file
+	"""
 	try:
 		with open(config_file, 'r') as config:
 			return json.loads(config.read())
@@ -13,6 +19,9 @@ def get_config():
 		return init_config()
 
 def init_config():
+	"""
+		Creates a new `scorpion.json`
+	"""
 	is_first_time = True
 	print('Creating a new "scorpion.json"\n')
 	origin = input('Enter path of files to copy (relative): ')
@@ -31,21 +40,69 @@ def init_config():
 		json.dump(new_config, new_config_file, indent=2)
 		return new_config
 
-def print_config(config):
-	for key in config:
-		print(f'[SCORPION] :: {key} :: {config[key]}')
+def log(line):
+	print(f'[SCORPION]\t::\tRunning\t$ {line}')
 
 def prompt_launch_on_init():
 	"""
-		If this is the first time the user runs scorpion,
-		ask before running
+		Prompt user before running file transfer
+		if config was created in this instance
 	"""
+	run_now = input('Enter "Y" to execute scorpion, any other key to abort.\n')
+	return run_now.lower() == 'y'
+
+def print_config(config):
+	"""
+		Prints loaded configuration
+	"""
+	for key in config:
+		print(f'[SCORPION]\t::\t{key}\t::\t{config[key]}')
+
+	if is_first_time:
+		return prompt_launch_on_init()
+
+def recreate(config):
+	"""
+		Removes files from the
+		current target
+	"""
+	target = config.get('target')
+	address = config.get('address')
+	user = config.get('user')
+
+	cmd_remove = f'ssh {user}@{address} rmdir {target}'
+	cmd_create = f'ssh {user}@{address} mkdir -p {target}'
+	log(cmd_remove)
+	os.system(cmd_remove)
+	log(cmd_create)
+	os.system(cmd_create)
+
+def copy_new_files(config):
+	origin = config.get('origin')
+	target = config.get('target')
+	address = config.get('address')
+	user = config.get('user')
+
+	cmd_copy = f'scp -r {origin} {user}@{address}:{target}'
+	log(cmd_copy)
+	os.system(cmd_copy)
 
 def start():
 	"""
 		Initializes files sharing
 	"""
 	config = get_config()
-	print_config(config)
+	run_now = print_config(config)
+
+	if not is_first_time:
+		recreate(config)
+		copy_new_files(config)
+	else:
+		if is_first_time and run_now:
+			recreate(config)
+			copy_new_files(config)
+		else:
+			print('scorpion exited')
+			return
 
 start()
